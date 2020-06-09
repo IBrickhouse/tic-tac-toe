574514553b2d9a784858a365f3e12e059f6f5f0c
Isaiah Brickhouse <brickhoi@oregonstate.edu>
2020-06-09 09:46:27 -0700

    game.rb: is not over if the bard is not full test passsing

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,29 @@ require_relative 'board'
 
 class Game
 	def board(board)
-		board = @board
+
 	end
 
 	def over?
+		#false
+
+		if full?
+			true
+		else
+			false
+		end
+	end
+
+	def full?
 		false
 	end
+
+	def tie?
+		false
+	end
+
+	def winner
+
+	end
+
 end
